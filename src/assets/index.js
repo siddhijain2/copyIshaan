3cdf06a3f1d0d8165524bed13393892363ec4fe9
muskan0202 <muskanagrawal.eee18@itbhu.ac.in>
2023-02-21 02:52:27 +0530

    footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,7 @@ import Logo from './Logo.png'
 import menu from './menu.png'
 import close from './close.png'
 import Speakwise from './Speakwise.jpeg'
+import slides from './slides.png'
+import github from './github.png'
 
-export { kalamkaari,hero,Enuncify,Logo,menu,close,Speakwise }
+export { kalamkaari,hero,Enuncify,Logo,menu,close,Speakwise,slides, github }
