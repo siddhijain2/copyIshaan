5f676e592239569fbd504a3cf8ce787c882b0e14
siddhi <siddhijain242000@gmail.com>
2023-02-21 02:21:55 +0530

    Resolved issues with routing

--- src/pages/Kalamkaari.jsx
+++ src/pages/Kalamkaari.jsx
@@ -1,8 +1,4 @@
-import '../App.css';
-import Card from '../components/GameCard';
-import styles from "../style";
-import { games } from '../constants';
-import Hero from '../components/Hero';
+
 
 function Kalamkaari() {
   return (
--- src/pages/Speakwise.jsx
+++ src/pages/Speakwise.jsx
@@ -1,8 +1,5 @@
 import '../App.css';
-import Card from '../components/GameCard';
-import styles from "../style";
-import { games } from '../constants';
-import Hero from '../components/Hero';
+
 
 function Speakwise() {
   return (
