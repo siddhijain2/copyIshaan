82149e63e1c5e73cbc777cb0b62da1504de4c404
divya-ilona <divyasinha.eee18@itbhu.ac.in>
2023-02-21 21:34:08 +0530

    removed dark theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import '../App.css';
 import styles from "../style";
 import { games, members } from '../constants';
-import {Hero,Card,About,FeatureCard} from '../components/Home';
+import {Hero,Card,About} from '../components/Home';
 import TeamCard from "../components/Home/Team";
 
 function Home() {
@@ -26,10 +26,10 @@ function Home() {
       <div class={`${styles.boxWidth} `}>
           <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6 ">
             <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-              <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">
                 Our team
               </h2>
-              <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+              <p class="font-light text-gray-500 sm:text-xl">
                 Explore the whole collection of open-source web components and
                 elements built with the utility classes from Tailwind
               </p>
