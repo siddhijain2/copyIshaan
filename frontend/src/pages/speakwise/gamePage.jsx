e836699430df0010d16fac1a7b475dd1e5f161f7
muskan0202 <muskanagrawal.eee18@itbhu.ac.in>
2023-03-09 03:38:27 +0530

    Result page for speakwise

--- frontend/src/pages/speakwise/gamePage.jsx
+++ frontend/src/pages/speakwise/gamePage.jsx
@@ -2,21 +2,19 @@ import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 import Recorder from "./recorder.jsx";
 import Button from "../../components/Button";
+import { CircularProgress } from "@mui/material";
 
 // import Timer from "../../components/Kalamkaari/Timer";
 // import { getWord, useTimer } from "../../services";
 
-
-
 function Speakwise() {
-  const [response, setResponse] = useState("original");
+  const [response, setResponse] = useState("");
   const [prompt, setPrompt] = useState("");
-
-    // const { timer, handleStart, handlePause } = useTimer();
-
+  const [isLoading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const API_KEY = process.env.REACT_APP_API_KEY;
     const configuration = new Configuration({
       apiKey: API_KEY,
@@ -34,6 +32,7 @@ function Speakwise() {
     });
     // console.log(res);
     setResponse(res.data.choices[0].text);
+    setLoading(false);
     // return res.data.choices[0].text;
   };
 
@@ -48,47 +47,39 @@ function Speakwise() {
     return words.length;
   }
 
-
   return (
-    <>
-      {/* <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nisi
-        nemo optio natus inventore modi esse at provident, beatae perferendis
-        temporibus odio doloremque! Unde ad asperiores qui enim accusamus
-        soluta.
+    <div className="h-screen flex flex-col justify-center items-center relative">
+      {/* <img className=" h-[90%] z-0 " src={Speakwiser} /> */}
+      <div class="flex items-center justify-center  absolute z-50 mt-50">
+        <div class="w-5/5 py-20  ">
+          <div class=" flex flex-col text-sm rounded-md">
+            <input
+              class="rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-yellow-500 "
+              onChange={(e) => setPrompt(e.target.value)}
+              value={prompt}
+              type="text"
+              placeholder="Enter topic for prompt"
+            />
+            {isLoading ? (
+              <CircularProgress />
+            ) : (
+              <button
+                class="mt-5 w-full border p-2 shadow:md bg-gradient-to-r from-Tomato to-ChiliRed text-white rounded-[4px] hover:bg-slate-400 scale-105 duration-300"
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Submit
+              </button>
+            )}
+          </div>
+          <h1 align="center"><b>{prompt}</b></h1>
+          <div>{response}</div>
+          <div>Total words = {countWords(response)}</div>
+          <Recorder />
+        </div>
       </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-        tempora facilis error dolore accusamus facere aut suscipit aliquam,
-        molestias blanditiis corrupti! Quo, dignissimos quia eum optio adipisci
-        dolorem doloribus sunt!
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-        tempora facilis error dolore accusamus facere aut suscipit aliquam,
-        molestias blanditiis corrupti! Quo, dignissimos quia eum optio adipisci
-        dolorem doloribus sunt!
-      </div>
- 
-
-      <div>{ {prompts} }</div> */}
-      <form onSubmit={handleSubmit}>
-        <input className="mt-40" placeholder="Enter topic for prompt"
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-
-      <div>{response}</div>
-      <div>Total words = {countWords(response)}</div>
-      <Recorder></Recorder>
-
-    </>
+    </div>
   );
 }
 
 export default Speakwise;
-
-// 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
--- frontend/src/pages/speakwise/resultPage.jsx
+++ frontend/src/pages/speakwise/resultPage.jsx
@@ -1,25 +1,77 @@
 // import fluency from ""../recorder.jsx";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { flu_advanced, flu_beginner, flu_intermediate } from "../../assets";
 
 function SpeakwiseResult() {
+  // var f = localStorage.getItem("fluency");
+  const [f, setF] = useState(localStorage.getItem("fluency"));
+  // console.log(f);
+  const [imgsrc, setImgsrc] = useState();
+  var wpm = Number(localStorage.getItem("wpm"));
+  useEffect(() => {
+    // f = localStorage.getItem("fluency");
+    setF(localStorage.getItem("fluency"));
+    // console.log(f);
+    if (f === "0") setImgsrc(flu_beginner);
+    if (f === "1") setImgsrc(flu_intermediate);
+    if (f === "2") setImgsrc(flu_advanced);
+  }, []);
+
   return (
     <>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laboriosam
-        culpa obcaecati id alias, similique, possimus deleniti cum error
-        repudiandae ipsa pariatur quidem labore. Voluptas dolorum eveniet vel
-        unde ex!
-      </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
-        tempore sapiente magni minima sunt tempora placeat repudiandae quos,
-        autem dignissimos commodi quidem beatae sint provident vel, sit
-        voluptate voluptates dolores.
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem illum
-        aperiam hic ipsa reprehenderit, at fugiat eos dolores ullam architecto
-        consequatur ab magni. Recusandae numquam nisi tenetur? Asperiores, sed
-        accusantium.
+      {/* {console.log(f)} */}
+      {/* {console.log(typeof f)} */}
+      <div className="mt-40">
+        {f === "0" ? (
+          <>
+            <img
+              src={imgsrc}
+              width="50%"
+              height="auto"
+              class="flex items-center justify-center"
+            />
+          </>
+        ) : f === "1" ? (
+          <>
+            <img
+              src={imgsrc}
+              width="50%"
+              height="auto"
+              class="flex items-center justify-center"
+            />
+          </>
+        ) : f === "2" ? (
+          <>
+            <img
+              src={imgsrc}
+              width="50%"
+              height="auto"
+              class="flex items-center justify-center"
+            />
+          </>
+        ) : (
+          <div> Record audio to get fluency</div>
+        )}
+
+        <div>Words per minute = {wpm}</div>
+
+        {wpm<120 ? (
+          <>
+            <div> Try speaking a bit faster.</div>
+          </>
+        ) : wpm>120 ? (
+          <>
+            <div>Try speaking a bit slower.</div>
+          </>
+        ) : (
+          <div> Speed is perfect.</div>
+        )}
+        <Link to="/speakwise">
+          <button class="mt-5 border p-2 shadow:md bg-gradient-to-r from-Tomato to-ChiliRed text-white rounded-[4px] hover:bg-slate-400 scale-105 duration-300">
+            Try again
+          </button>
+        </Link>
       </div>
     </>
   );
