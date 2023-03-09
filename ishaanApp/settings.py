e805cdd6eb8439dc4d39a81ba48218657ade963c
divya-ilona <divyasinha.eee18@itbhu.ac.in>
2023-03-10 00:16:59 +0530

    deployed

--- ishaanApp/settings.py
+++ ishaanApp/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
 AUTH_USER_MODEL = 'account.User'
 
