16b7d47e039780464d8cb908654b4d58e6d70413
divya-ilona <divyasinha.eee18@itbhu.ac.in>
2023-03-10 01:28:42 +0530

    finally deployed

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,23 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
-import { Provider } from "react-redux";
-import { store } from "./pages/enuncify/reducers/store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './App.css'
+import { Provider } from 'react-redux'
+import { store } from './pages/enuncify/reducers/store'
 import { reducer as reducer2, store2 } from './store'
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import * as serviceWorker from './serviceWorker';
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import * as serviceWorker from './serviceWorker'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 function Root() {
   return (
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-  );
+  )
 }
-root.render(<Root/>);
-
-
-serviceWorker.unregister();
+root.render(<Root />)
--- ishaanApp/settings.py
+++ ishaanApp/settings.py
@@ -50,15 +50,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
